f53f3d8b2a896d6f94b168d983e86b824e0f2732
lucastort1 <159059797+lucastort1@users.noreply.github.com>
2024-10-02 20:10:57 -0300

    codigo-ponderada

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,11 +1,15 @@
-import numpy as np
+import numpy as np  # Importa a biblioteca NumPy para facilitar operações matemáticas e manipulação de arrays
 
+# Define a função calculate
 def calculate(numbers):
+    # Verifica se a lista contém exatamente 9 números, caso contrário, levanta um erro
     if len(numbers) != 9:
         raise ValueError("A lista deve conter nove números")
-    
+     
+    # Converte a lista de números em um array NumPy e o remodela para uma matriz 3x3
     array = np.array(numbers).reshape(3, 3)
-    
+
+    # Calcula várias estatísticas para a matriz
     calculations = {
         'mean': [np.mean(array, axis=0).tolist(), np.mean(array, axis=1).tolist(), np.mean(array).tolist()],
         'variance': [np.var(array, axis=0).tolist(), np.var(array, axis=1).tolist(), np.var(array).tolist()],
@@ -14,5 +18,6 @@ def calculate(numbers):
         'min': [np.min(array, axis=0).tolist(), np.min(array, axis=1).tolist(), np.min(array).tolist()],
         'sum': [np.sum(array, axis=0).tolist(), np.sum(array, axis=1).tolist(), np.sum(array).tolist()]
     }
-    
+
+    # Retorna um dicionário contendo todas as estatísticas calculadas
     return calculations
